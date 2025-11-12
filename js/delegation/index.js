9c10badb5de7f35a4ddface3802938a541411f64
yurij <mihov.yurij@gmail.com>
2025-11-12 21:07:01 +0200

    update homework

--- js/delegation/index.js
+++ js/delegation/index.js
@@ -1,4 +1,19 @@
 "use strict"
+function setupEventDelegation(selector) {
+ 
+  const list = typeof selector === 'string' ? document.querySelector(selector) : selector;
+  if (!list) {
+    console.log('List is found for selector:', selector);
+    return;
+  }
+  list.addEventListener('click', function(event) {
+       const li = event.target.closest('li');
+        if (!li || !list.contains(li)) return;
+    console.log(`Item clicked: ${li.textContent}`);
+  });
+}
+
+
 function createTestList() {
   document.body.innerHTML = `
     <ul id="testList">
@@ -6,18 +21,8 @@ function createTestList() {
       <li>Item 2</li>
       <li>Item 3</li>
     </ul>
-    `
+  `;
 }
-createTestList()
-
-function setupEventDelegation(selector) {
-  const list = document.querySelector(selector)
-  list.addEventListener('click', function(event) {
-    if (event.target.tagName === 'LI') {
-        console.log(`Item clicked: ${event.target.textContent}`)
-    }
-  })
-}
-
-setupEventDelegation('#testList');
+createTestList();
+setupEventDelegation('#testList'); 
 export { setupEventDelegation };
